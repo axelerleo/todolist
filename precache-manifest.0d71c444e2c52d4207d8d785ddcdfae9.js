b8948a9aadca06a9c2787247f70999f0d654735b
Alexey <axelerleo@yandex.ru>
2019-05-02 22:51:04 +0300

    Updates

---
+++ precache-manifest.0d71c444e2c52d4207d8d785ddcdfae9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c18d3d6cc3d286ff83d4d3e727c5f0",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "18f12b9d75cb8d47381b",
+    "url": "/todolist/static/css/main.f25d1213.chunk.css"
+  },
+  {
+    "revision": "ea509b68824308472ba9",
+    "url": "/todolist/static/js/2.b962a288.chunk.js"
+  },
+  {
+    "revision": "18f12b9d75cb8d47381b",
+    "url": "/todolist/static/js/main.b2ab5829.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
